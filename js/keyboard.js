61d02c7cba2a19bb54dae066498b79b17ebaa629
Konrad <fprognoz@gmail.com>
2023-05-01 15:03:17 +0300

    feat: add layout for shift state in English

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,7 +9,7 @@ class Keyboard {
 
   async loadKeys() {
     try {
-      const response = await fetch('js/keyboard-en.json');
+      const response = await fetch('js/keyboard.json');
       this.ALL_KEYS = await response.json();
     } catch (error) {
       console.error(error);
@@ -20,18 +20,23 @@ class Keyboard {
     const keyboard = document.createElement('main');
     const lines = [];
     let currentLine = [];
-    keyboard.className = 'keyboard';
+    keyboard.className = 'keyboard keyboard_shift';
 
     this.ALL_KEYS.forEach((key) => {
       const button = document.createElement('button');
       button.className = key.style === '' ? 'key' : `key ${key.style}`;
       const mainSpan = document.createElement('span');
       mainSpan.className = 'key__main';
-      mainSpan.textContent = key.mainValue;
-      const secondSpan = document.createElement('span');
-      secondSpan.className = 'key__little';
-      secondSpan.textContent = key.secondValue;
-      button.append(mainSpan, secondSpan);
+      mainSpan.textContent = key.system ? key.value : key.lang.en.mainValue;
+      button.append(mainSpan);
+      if (!key.system) {
+        const secondSpan = document.createElement('span');
+        secondSpan.className = 'key__little';
+        secondSpan.textContent = key.lang.en.secondValue;
+        button.append(secondSpan);
+      } else {
+        mainSpan.classList.add('key__system');
+      }
       currentLine.push(button);
       if (key.last) {
         lines.push(currentLine);
