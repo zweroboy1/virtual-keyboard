195679c3e83207364fde93e2f135cc086e6e37fd
Konrad <fprognoz@gmail.com>
2023-05-01 16:07:50 +0300

    feat: add all css styles for en/ru, shift/normal mode

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
 import { Keyboard } from './keyboard.js';
 
 const keyboard = new Keyboard(document.body);
-// keyboard.render();
--- js/keyboard.js
+++ js/keyboard.js
@@ -20,23 +20,37 @@ class Keyboard {
     const keyboard = document.createElement('main');
     const lines = [];
     let currentLine = [];
-    keyboard.className = 'keyboard'; // keyboard_shift';
+    keyboard.className = 'keyboard keyboard_shift keyboard_ru';
 
     this.ALL_KEYS.forEach((key) => {
       const button = document.createElement('button');
       button.className = key.style === '' ? 'key' : `key ${key.style}`;
+
       const mainSpan = document.createElement('span');
-      mainSpan.className = 'key__main';
-      mainSpan.textContent = key.system ? key.value : key.lang.ru.mainValue;
+      mainSpan.classList.add('key__main');
       button.append(mainSpan);
-      if (!key.system) {
-        const secondSpan = document.createElement('span');
-        secondSpan.className = 'key__little';
-        secondSpan.textContent = key.lang.ru.secondValue;
-        button.append(secondSpan);
+      if (key.system) {
+        mainSpan.classList.add('key_system');
+        mainSpan.textContent = key.value;
       } else {
-        mainSpan.classList.add('key__system');
+        mainSpan.classList.add('key_en');
+        mainSpan.textContent = key.lang.en.mainValue;
+
+        const secondSpan = document.createElement('span');
+        secondSpan.className = 'key__little key_en';
+        secondSpan.textContent = key.lang.en.secondValue;
+
+        const mainSpanRu = document.createElement('span');
+        mainSpanRu.className = 'key__main key_ru';
+        mainSpanRu.textContent = key.lang.ru.mainValue;
+
+        const secondSpanRu = document.createElement('span');
+        secondSpanRu.className = 'key__little key_ru';
+        secondSpanRu.textContent = key.lang.ru.secondValue;
+
+        button.append(secondSpan, mainSpanRu, secondSpanRu);
       }
+
       currentLine.push(button);
       if (key.last) {
         lines.push(currentLine);
